dd9c95dcf09d2e0b3a4e4d2f8cb131688e9c96ae
Riotters <krzysztof@riotters.com>
2023-02-28 22:20:35 +0100

    Update sponsors.jsx

--- src/atomic/sections/homepage/sponsors.jsx
+++ src/atomic/sections/homepage/sponsors.jsx
@@ -1,7 +1,7 @@
 import * as React from "react"
 import styled from "styled-components"
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import "slick-carousel/slick/slick.css"
+import "slick-carousel/slick/slick-theme.css"
 import Container from "../../partials/container"
 import Img from "gatsby-image"
 import { StaticImage } from "gatsby-plugin-image"
@@ -10,84 +10,105 @@ import { FlexBox } from "../../../components/flexbox"
 import Grid from "../../../components/grid"
 
 const Sponsors = styled.section`
-    position: relative;
+  position: relative;
 
-    .slick-list {
-        overflow: visible;
-    }
+  .slick-list {
+    overflow: visible;
+  }
 
-    .slick-dots {
-        bottom: -65px;
+  .slick-dots {
+    bottom: -65px;
 
-        li {
-            margin: 0;
+    li {
+      margin: 0;
 
-            button::before {
-                color: ${color.neutral20};
-                font-size: 12px;
-                opacity: 1;
-            }
+      button::before {
+        color: ${color.neutral20};
+        font-size: 12px;
+        opacity: 1;
+      }
 
-            &.slick-active {
-                button::before {
-                    color: ${color.red40};
-                    opacity: 1;
-                }
-            }
+      &.slick-active {
+        button::before {
+          color: ${color.red40};
+          opacity: 1;
         }
+      }
     }
+  }
 
+  .left-bg {
+    display: none;
+    position: absolute;
+    left: 0;
+    bottom: -210px;
+    z-index: -1;
+    mix-blend-mode: lighten;
+
+    &::before {
+      content: "";
+      display: block;
+      padding-top: 256.8%;
+    }
+  }
+
+  .sponsors {
+    img {
+      position: static !important;
+      object-fit: contain !important;
+    }
+  }
+
+  @media (min-width: 769px) {
     .left-bg {
-        display: none;
-        position: absolute;
-        left: 0;
-        bottom: -210px;
-        z-index: -1;
-        mix-blend-mode: lighten;
-
-        &::before {
-            content: '';
-            display: block;
-            padding-top: 256.8%;
-        }
+      display: block;
     }
-
-    .sponsors {
-        img {
-            position: static !important;
-            object-fit: contain !important;
-        }
-    }
-
-    @media (min-width: 769px) {
-        .left-bg {
-            display: block;
-        }
-    }
+  }
 `
 
 const Anchor = styled.span`
-    position: absolute;
-    top: -182px;
+  position: absolute;
+  top: -182px;
 `
 
-
 const SponsorsSection = ({ className, data }) => (
-    <Sponsors className={className} imageWidth={data.sponsorzyZdjecia.length}>
-        <Anchor id="sponsorzy" />
-        <Container>
-            <Grid className="sponsors" columnsMD={`repeat(${data.sponsorzyZdjecia.length}, minmax(25%, calc(50% - 12px)))`} gap="24px" justify="center" width="100%" m="64px 0 0" mMD="172px 0 0">
-                {data.sponsorzyZdjecia.map((zdjecie) => (
-                    <a href={zdjecie.sponsorzyZdjeciaLink ? zdjecie.sponsorzyZdjeciaLink : null} target="_blank">
-                        <FlexBox className="content-left" width="100%" relative >
-                            <Img fixed={zdjecie.sponsorzyZdjeciaZdjecie.localFile.childImageSharp.fluid} />
-                        </FlexBox>
-                    </a>
-                ))}
-            </Grid>
-        </Container>
-        <StaticImage className="left-bg" src="../../../images/1.png" width="430px" />
-    </Sponsors >
+  <Sponsors className={className} imageWidth={data.sponsorzyZdjecia.length}>
+    <Anchor id="sponsorzy" />
+    <Container>
+      <Grid
+        className="sponsors"
+        columnsMD={`repeat(${data.sponsorzyZdjecia.length}, minmax(25%, calc(50% - 12px)))`}
+        gap="24px"
+        justify="center"
+        width="100%"
+        m="64px 0 0"
+        mMD="172px 0 0"
+      >
+        {data.sponsorzyZdjecia?.map(zdjecie => (
+          <a
+            href={
+              zdjecie.sponsorzyZdjeciaLink ? zdjecie.sponsorzyZdjeciaLink : null
+            }
+            target="_blank"
+          >
+            <FlexBox className="content-left" width="100%" relative>
+              <Img
+                fixed={
+                  zdjecie.sponsorzyZdjeciaZdjecie.localFile.childImageSharp
+                    .fluid
+                }
+              />
+            </FlexBox>
+          </a>
+        ))}
+      </Grid>
+    </Container>
+    <StaticImage
+      className="left-bg"
+      src="../../../images/1.png"
+      width="430px"
+    />
+  </Sponsors>
 )
 
 export default SponsorsSection
